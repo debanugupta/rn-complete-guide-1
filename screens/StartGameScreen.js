b81d387415f5f3f87e9ee4f07fdfb83b760eccea
Debanu Gupta <debanugupta@gmail.com>
2021-11-19 13:07:16 +0530

    67. Switching Between Multiple "Screens"

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,17 +6,20 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert
 } from 'react-native';
+
 import Card from '../components/Card';
 import Input from '../components/Input';
-import Color from '../constants/color';
+import NumberContainer from '../components/NumberContainer';
+import Colors from '../constants/colors';
 
-const StartGameScreen = (props) => {
+const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
 
-  const numberInputHandler = (inputText) => {
+  const numberInputHandler = inputText => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''));
   };
 
@@ -27,18 +30,30 @@ const StartGameScreen = (props) => {
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
-    if (chosenNumber === NaN || chosenNumber <=0 || chosenNumber > 99) {
-        return;
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      return;
     }
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue('');
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-      confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
+      </Card>
+    );
   }
 
   return (
@@ -66,14 +81,14 @@ const StartGameScreen = (props) => {
               <Button
                 title="Reset"
                 onPress={resetInputHandler}
-                color={Color.accent}
+                color={Colors.accent}
               />
             </View>
             <View style={styles.button}>
               <Button
                 title="Confirm"
                 onPress={confirmInputHandler}
-                color={Color.primary}
+                color={Colors.primary}
               />
             </View>
           </View>
@@ -88,30 +103,34 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   title: {
     fontSize: 20,
-    marginVertical: 10,
+    marginVertical: 10
   },
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   buttonContainer: {
     flexDirection: 'row',
     width: '100%',
     justifyContent: 'space-between',
-    paddingHorizontal: 15,
+    paddingHorizontal: 15
   },
   button: {
-    width: 100,
+    width: 100
   },
   input: {
     width: 50,
-    textAlign: 'center',
+    textAlign: 'center'
   },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: 'center'
+  }
 });
 
 export default StartGameScreen;
